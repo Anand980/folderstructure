f8f882abde22673c0917b29a1f9187d63f2d6156
Anand980 <anand.bharti980@gmail.com>
2022-07-14 13:40:47 +0530

    Create folderData.js

---
+++ data/folderData.js
@@ -0,0 +1,36 @@
+const explorer = {
+  name: "root",
+  isFolder: true,
+  items: [
+    {
+      name: "public",
+      isFolder: true,
+      items: [
+        {
+          name: "index.html",
+          isFolder: false
+        }
+      ]
+    },
+    {
+      name: "src",
+      isFolder: true,
+      items: [
+        {
+          name: "App.js",
+          isFolder: false
+        },
+        {
+          name: "index.js",
+          isFolder: false
+        }
+      ]
+    },
+    {
+      name: "package.json",
+      isFolder: false
+    }
+  ]
+};
+
+export default explorer;
